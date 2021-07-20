2bf2e6f8c1ca590bc62519c943734ef49193c698
avaniw <hccstudent1221@hotmail.com>
2021-07-20 16:06:26 -0400

    file structure

---
+++ index.js
@@ -0,0 +1,5 @@
+//require manager class from the lib folder
+//require engineer class from the lib folder
+//require intern class from the lib folder
+const path = require("path");
+const fs = require("fs");
